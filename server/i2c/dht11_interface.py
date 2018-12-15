84995d34a442070054bf7feddf41692b1f1bff43
saii <saikat626@gmail.com>
2018-12-15 20:06:38 +0530

    Using python to do i2c as node requires admin privileges. (Not good solution. Will change later. Need to create a interface for around python endpoints to work :(). Added code to get data from dht11 temperature and humidity sensor

---
+++ server/i2c/dht11_interface.py
@@ -0,0 +1,26 @@
+import sys
+import RPi.GPIO as GPIO
+import dht11
+import time
+import json
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+pin = int(sys.argv[1])
+instance = dht11.DHT11(pin)
+
+while True:
+    result = instance.read()
+    if result.is_valid():
+        data = {}
+        data['time'] = time.time()
+        data['temperature'] = result.temperature
+        data['humidity'] = result.humidity
+        print(json.dumps(data))
+        sys.stdout.flush()
+
+    time.sleep(1)
