c45999076161818c222d76ceedcdaf6421c67c14
saii <saikat626@gmail.com>
2018-12-23 03:30:58 +0530

    Added news fetcher. Checks msg buffer for full before adding to displayMessage

--- server/scripts/lcd.js
+++ server/scripts/lcd.js
@@ -1,5 +1,6 @@
 const pythonShell = require('python-shell');
 const path = require('path');
+const request = require('request');
 
 var options = {
   mode: 'text',
@@ -135,18 +136,27 @@ function info() {
         line: 'String' // display line on Line 2 of LCD
       }
     }],
-    working: 'Shows the string on specified line'
+    working: 'Shows the string'
+  }
+}
+
+class Message {
+  constructor(params) {
+    this.msg = params.msg || null;
+    this.lines = params.lines || null;
+    this.duration = (params.duration && params.duration < 10) ? params.duration : 5;
+    this.repeat = (params.repeat && params.repeat < 10) ? params.repeat : 1;
+    this._noOfMessages = this.msg ? Math.ceil(this.msg.length / 40) : Math.ceil(this.lines.length / 2);
+    this._totalTime = this._noOfMessages * this.duration;
   }
 }
 
 function displayMsg(rpio, params) {
   if (params.msg) {
-    let msg = params.msg;
-    if (msg.msg && msg.msg.length > 0) {
-      msg.duration = (msg.duration && msg.duration < 10) ? msg.duration : 5;
-      msg.repeat = (msg.repeat && msg.repeat < 5) ? msg.repeat : 1;
-      displayMessages.push(msg);
-      return msg;
+    if ((params.msg && params.msg.length > 0) || (params.lines && params.lines.length > 0)) {
+      let message = new Message(params);
+      checkAndAddMsgToDisplayLoop(message);
+      return message;
     } else {
       return {
         status: 'error',
@@ -154,6 +164,32 @@ function displayMsg(rpio, params) {
       }
     }
   }
+}
+
+function checkAndAddMsgToDisplayLoop(msg) {
+  let totalMsgDuration = 0;
+  displayMessages.forEach((message) => {
+    totalMsgDuration = totalMsgDuration + message._totalTime * message.repeat;
+  });
+
+  totalMsgDuration = totalMsgDuration + msg._totalTime * msg.repeat;
+
+  if (totalMsgDuration > 86400) {
+    let postData = {
+      title: "LCD Display",
+      body: "Msg Buffer 1 day timeout reached. Resetting buffer"
+    }
+    request.post('https://saikat.app/notify/zuk', {
+      form: postData
+    }, function(err, res, body) {
+      if (err) {
+        console.error(err);
+      }
+    });
+
+    displayMessages = [];
+  }
+  displayMessages.push(msg);
 }
 
 // Line 2 : Usaually to show temperature and humidity
